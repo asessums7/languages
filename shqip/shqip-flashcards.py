f4ebefab3b8a262c11a0ccc8513b4afa9cfe889f
asessums7 <asessums7@gmail.com>
2022-08-26 14:05:28 +0200

    completed shqip flashcards

---
+++ shqip/shqip-flashcards.py
@@ -0,0 +1,134 @@
+### SHQIP-E
+### Alfabeti Shqip - Language Learning Flaschard Set
+
+# %% [markdown]
+# Developed by Alex Sessums
+# August 2022
+
+# %%
+# Import Libraries
+import pandas as pd
+from tkinter import *
+from random import randint
+from art import tprint
+
+# print logo
+print ("\n\n\n" + '\033[94m')
+tprint('SHQIP-E')
+
+# welcome message
+print ('\033[94m' + "Përshëndetje!\n\nThis is a little Python program that will help you learn Albanian words and phrases.\nSelect your preferences below to begin learning.\n\nType", '\033[1m', "exit" + '\033[0m' + '\033[94m'  + "  to exit\n\n")
+print ("\n")
+print ("---------------------------------")
+print ("\n")
+
+# %%
+## User input 1, requesting which flashcard set they would like to study.
+content = input('\033[94m' + 'First, what would you like to study today? Select from these options: \n \n    1. Numbers\n    2. Colors\n    3. Food\n    4. Household\n    5. Clothing\n    6. Family\n    7. Weather\n    8. Transport\n \nFor a flashcard set of all words, type "dictionary" ' + '\033[95m')
+print(content)
+
+## User input 2, requesting word order. i.e - English to Albanian or Albanian to English.
+
+
+# Flashcard Formatting
+root = Tk()
+root.title("Alfabeti Shqip Flashcards")
+root.geometry("800x500")
+
+# %%
+# Read in data from text files
+df = pd.read_csv(f'{content}.txt', sep = ",", index_col=1)
+df = df.reset_index()
+# df.columns = ["English", "Shqip", "Unnamed: 2"]
+df.drop(['Unnamed: 2'], axis=1, inplace=True)
+values = df.values
+words = values.tolist()
+
+# %%
+# Length of Words List
+count = len(words)
+
+# %%
+# Define a function to create a random word
+def next():
+    global hinter, hint_count
+    '''Clear the screen'''
+    answer_label.config(text="")
+    my_entry.delete(0, END)
+    hint_label.config(text = "")
+    # Reset hint
+    hinter = ""
+    hint_count = 0
+    
+    '''Create a global variable as a random selection'''
+    global random_word
+    random_word = randint(0, count-1)
+    '''Update the label with a Shqip word'''
+    shqip_word.config(text=words[random_word][0])
+    
+# %%
+# Define a function to return the answer
+def answer():
+    if my_entry.get().capitalize() == words[random_word][1]:
+        answer_label.config(text=f"Correct! {words[random_word][0]} is {words[random_word][1]}")
+    else:
+        answer_label.config(text=f"Incorrect! {words[random_word][0]} is not {my_entry.get().capitalize()}")
+
+# %%
+# Define a function to give a hint
+hinter = ""
+hint_count = 0
+
+def hint():
+    global hint_count
+    global hinter
+
+    if hint_count < len(words[random_word][1]):
+        hinter = hinter + words[random_word][1][hint_count]
+        hint_label.config(text = hinter)
+        hint_count += 1
+
+# %%
+# Get a count of the word list
+shqip_word = Label(root, text="", font=("TW Cen MT", 50))
+shqip_word.pack(pady=50)
+
+answer_label = Label(root, text="")
+answer_label.pack(pady=20)
+
+info = StringVar()
+my_entry = Entry(root, textvariable=info, font=("TW Cen MT", 35))
+my_entry.pack(pady=20)
+my_entry.get()
+
+# str(EntryAnswer)
+# EntryAnswer.pack(pady=20)
+
+# %%
+# Create Answer, Next & Hint Buttons
+button_frame = Frame(root)
+button_frame.pack(pady=20)
+
+answer_button = Button(button_frame, text="Answer", command=answer)
+answer_button.grid(row=0, column=0, padx=20)
+
+hint_button = Button(button_frame, text="Hint", command=hint)
+hint_button.grid(row=0, column=1)
+
+next_button = Button(button_frame, text="Next", command=next)
+next_button.grid(row=0, column=2, padx=20)
+
+# Create Hint Label
+hint_label = Label(root, text="")
+hint_label.pack(pady=40)
+
+# %%
+# Run next function when program starts
+next()
+
+# %%
+# Loop through the program to keep it running
+root.mainloop()
+
+# Resources
+# https://www.youtube.com/watch?v=Pd3XoLSQ5wg
